be7e5fd558f2924400ebaca1d8ca38dceff96dff
Dorancelis Cordones <doran162@hotmail.com>
2017-07-24 19:14:47 +0000

    search bottom

--- app/models/company.rb
+++ app/models/company.rb
@@ -3,5 +3,9 @@ class Company < ActiveRecord::Base
     validates :description, presence: true, length: { minimum: 10, maximum: 50 }
     validates :image, presence: true, length: { minimum: 3, maximum: 50 }
     has_many :items
+    
+    def self.search(search)
+        where("name LIKE ?", "%#{search}%")
+    end
 
 end
