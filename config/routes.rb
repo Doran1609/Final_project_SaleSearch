d4a195db47a6e93b834a325d0a5258f404ad64dd
Dorancelis Cordones <doran162@hotmail.com>
2017-07-25 15:17:12 +0000

    all again

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   
 
 
+  resources :profiles
+
   devise_for :users
   get 'cart/index'
 
@@ -17,6 +19,7 @@ Rails.application.routes.draw do
   get '/cart' => 'cart#index' 
   get '/cart/:id' => 'cart#add'
   get '/cart/clear' => 'cart#clearCart'
+  get '/signedinuserprofile' => 'profiles#signedinuserprofile'
 
 
    resources :companies do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170724192022) do
+ActiveRecord::Schema.define(version: 20170724193926) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -34,6 +34,21 @@ ActiveRecord::Schema.define(version: 20170724192022) do
   end
 
   add_index "items", ["company_id"], name: "index_items_on_company_id"
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "title"
+    t.string   "firstname"
+    t.string   "lastname"
+    t.string   "phone"
+    t.string   "address"
+    t.string   "zipcode"
+    t.string   "city"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
