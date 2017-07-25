385327dc703d79679f611e7b5a358d08641d1008
Dorancelis Cordones <doran162@hotmail.com>
2017-07-25 21:59:04 +0000

    Admin working

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,4 +1,6 @@
 class CompaniesController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :ensure_admin, :only => [:edit, :destroy]
   before_action :set_company, only: [:show, :edit, :update, :destroy]
 
   # GET /companies
@@ -30,6 +32,12 @@ class CompaniesController < ApplicationController
   # POST /companies
   # POST /companies.json
   def create
+    
+    
+    puts "debug +++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    puts params.inspect
+    puts "debug +++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    
     @company = Company.new(company_params)
 
     respond_to do |format|
@@ -67,6 +75,14 @@ class CompaniesController < ApplicationController
     end
   end
 
+
+  def ensure_admin
+    unless current_user && current_user.admin?
+      render :text => "Access Error Message", :status => :unauthorized
+    end
+  end
+  
+  
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_company
