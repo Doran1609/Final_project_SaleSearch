4f2dc11a41aa7104a0349cf8b3476e1d13eec49b
Dorancelis Cordones <doran162@hotmail.com>
2017-07-31 21:55:08 +0000

    updating styling

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,13 @@ class ItemsController < ApplicationController
     # Access all items for that company
     @items = @company.items
     
+    if params[:search]
+      @items = Item.search(params[:search]).order("created_at DESC")
+      
+    else
+      @items = Item.all.order('created_at DESC')
+    end
+    
   end
   
   
